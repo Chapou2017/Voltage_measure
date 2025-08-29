7eb3bbe7e7ea7a5a23713d1babd0cd72654bf092
Olivier Poussin <opoussin@yahoo.fr>
2025-08-29 22:11:15 +0200

    Voltage measure OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,43 @@
+//Si tu veux améliorer la précision :
+//
+//    Ajoute un condensateur (ex. 100nF) entre la broche analogique et la masse pour stabiliser la lecture.
+//
+//    Utilise analogReadResolution(12); dans setup() pour t’assurer que la résolution est bien à 12 bits.
+
 #include <Arduino.h>
 
-const int analogPin = 32; // Choisis une broche analogique stable
-const float R1 = 10000.0; // 10kΩ
-const float R2 = 4700.0;  // 4.7kΩ
+// Définition des broches 
+const int analogPin = 33; // Choisis une broche analogique stable
+
+// Définition des variables
+const float R1 = 30000.0; // 30kΩ
+const float R2 = 7500.0;  // 7.5kΩ
+float tension_mesure = 0.00;
+int rawValue = 0;
+float tension_moteur = 0.00;
+
+float corrigerTension(float tension_lue) {
+  return 1.016 * tension_lue + 0.71;
+}
 
 void setup() {
   Serial.begin(115200);
+  analogReadResolution(12);
 }
 
 void loop() {
-  int rawValue = analogRead(analogPin);
-  float voltage = (rawValue / 4095.0) * 3.3; // Conversion en tension réelle
-  float Vin = voltage * ((R1 + R2) / R2);    // Recalcul de la tension d’entrée
+  rawValue = analogRead(analogPin);
+  tension_mesure = (rawValue / 4095.0) * 3.3; // Conversion en tension réelle
+  tension_moteur = tension_mesure * ((R1 + R2) / R2);    // Recalcul de la tension d’entrée
+  tension_moteur = corrigerTension(tension_moteur);
 
-  Serial.print("Tension mesurée : ");
-  Serial.print(Vin);
+  Serial.print("Tension pin33: ");
+  Serial.print(tension_mesure);
   Serial.println(" V");
+    Serial.print("Tension mesurée : ");
+  Serial.print(tension_moteur);
+  Serial.println(" V");
+  Serial.println();
 
   delay(500);
 }
